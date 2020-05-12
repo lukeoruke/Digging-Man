3eeef6d16d60bc7e3de0ad425d62073c27e181c0
AndrewOki <65211402+AndrewOki@users.noreply.github.com>
2020-05-12 03:07:47 -0700

    Add files via upload

---
+++ Actor.h
@@ -0,0 +1,15 @@
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+#include "GraphObject.h"
+
+
+//inherit from graphobject
+class Ice  {
+public:
+
+};
+
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+#endif // ACTOR_H_
---
+++ StudentWorld.h
@@ -0,0 +1,44 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include "GameConstants.h"
+#include <string>
+
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld    //inherited from public gameworld
+{
+public:
+	StudentWorld(std::string assetDir)
+		: GameWorld(assetDir)
+	{
+	}
+
+	virtual int init(){
+		class Ice {
+		public:
+			void createIce();
+		private:
+
+	};
+	
+		return GWSTATUS_CONTINUE_GAME;
+	}
+
+	virtual int move()
+	{
+		// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
+		// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+		decLives();
+		return GWSTATUS_PLAYER_DIED;
+	}
+
+	virtual void cleanUp()
+	{
+	}
+
+private:
+};
+
+#endif // STUDENTWORLD_H_
