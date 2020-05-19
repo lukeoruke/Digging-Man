f40bd8cd80eedacf97489519a69ad447395670e8
AndrewOki <65211402+AndrewOki@users.noreply.github.com>
2020-05-18 18:26:22 -0700

    Update Actor.cpp

--- IceMan/IceMan/Actor.cpp
+++ IceMan/IceMan/Actor.cpp
@@ -18,19 +18,26 @@ const int ICEMAN_START_Y = 60;
 						  ////ICEMAN   PEOPLE	    EXC
 
 //////////////////////// ACTOR //////////////////  pg 24
-Actor::Actor(int imageID, int startX, int startY, Direction dir, double size, int depth)
+Actor::Actor(StudentWorld* world, int imageID, int startX, int startY, Direction dir, double size, int depth)
 	:GraphObject(imageID, startX, startY, dir, size, depth)
 {
+	m_world = world;
 	setVisible(true);
 }
-void Actor :: doSomething() {}
+//void Actor :: doSomething() {}
 
 StudentWorld* Actor::getWorld() const{
 	return  m_world;
 }
+
+bool Actor::setisAlive(bool status) {
+	return m_isAlive = status;
+}
+bool Actor::getisAlive() {
+	return m_isAlive;
+}
 Actor::~Actor(){
 }
-
 //////////////////////// ICE //////////////////
 Ice::Ice(int row, int col) :GraphObject(IID_ICE,row,col,right,0.25,3) {
 	setVisible(true);
@@ -39,8 +46,8 @@ Ice::~Ice() {}
 void Ice::doSomething(){};
 
 //////////////////////// ICEMAN //////////////////     pg 27
-Iceman::Iceman()
-	:Actor(IID_PLAYER, ICEMAN_START_X, ICEMAN_START_Y, right, 1.0, 0)
+Iceman::Iceman(StudentWorld* world)
+	:Actor(world,IID_PLAYER, ICEMAN_START_X, ICEMAN_START_Y, right, 1.0, 0)
 {
 	m_HP = 10;
 	m_water_amnt = 5;
@@ -60,7 +67,9 @@ Iceman::~Iceman(){}
 //}
 
 void Iceman::doSomething() {
+	//check if the iceman is alive
 	if (m_HP <= 0) {
+		setisAlive(false);
 		return;
 	} 
 
@@ -69,15 +78,16 @@ void Iceman::doSomething() {
 	if (getWorld()->getKey(ch) == true) {
 		switch(ch)
 		{
-			case KEY_PRESS_LEFT:
-				//move player left here...
+			case KEY_PRESS_LEFT:  //x-1
+				setDirection(left);
+				moveTo(getX()- 1, getY());
 				break;
-			case KEY_PRESS_RIGHT:
-				//move player right here...
+			case KEY_PRESS_RIGHT: //x+1
+				setDirection(right);
 				break;
-			case KEY_PRESS_DOWN:
+			case KEY_PRESS_DOWN:  //y-1
 				break;
-			case KEY_PRESS_UP:
+			case KEY_PRESS_UP: //y+1
 				break;
 			case KEY_PRESS_SPACE:
 				break;
