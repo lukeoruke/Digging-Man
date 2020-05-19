603a8939efe6f32f564bf61a69f48745555d0afd
AndrewOki <65211402+AndrewOki@users.noreply.github.com>
2020-05-18 23:49:12 -0700

    Update Actor.cpp

--- IceMan/IceMan/Actor.cpp
+++ IceMan/IceMan/Actor.cpp
@@ -1,21 +1,21 @@
 #include "Actor.h"
 #include "StudentWorld.h"
-
+#include "GameController.h"
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
 const int ICEMAN_START_X = 30;
 const int ICEMAN_START_Y = 60;
 
 
 
-///GRAPH OBJ
-///    |
-//////________
-/////|      |
-////ICE		Actor	
-////		 |
-////_____________________
-////|		|            |
-////ICEMAN   PEOPLE	    EXC
+								///GRAPH OBJ
+								///    |
+							//////________
+							/////|      |
+							////ICE		Actor	
+							////		 |
+							////_____________________
+							////|		|            |
+						  ////ICEMAN   PEOPLE	    EXC
 
 //////////////////////// ACTOR //////////////////  pg 24
 Actor::Actor(StudentWorld* world, int imageID, int startX, int startY, Direction dir, double size, int depth)
@@ -26,86 +26,89 @@ Actor::Actor(StudentWorld* world, int imageID, int startX, int startY, Direction
 }
 //void Actor :: doSomething() {}
 
-StudentWorld* Actor::getWorld() const {
+StudentWorld* Actor::getWorld() const{
 	return  m_world;
 }
 
-void Actor::setIsAlive(bool status) {
-	m_isAlive = status;
+bool Actor::setisAlive(bool status) {
+	return m_isAlive = status;
 }
-bool Actor::getIsAlive() {
+bool Actor::getisAlive() {
 	return m_isAlive;
 }
-Actor::~Actor() {
+Actor::~Actor(){
 }
 //////////////////////// ICE //////////////////
-Ice::Ice(int row, int col) :GraphObject(IID_ICE, row, col, right, 0.25, 3) {
+Ice::Ice(int row, int col) :GraphObject(IID_ICE,row,col,right,0.25,3) {
 	setVisible(true);
 }
 Ice::~Ice() {}
-//void Ice::doSomething() {};
+//void Ice::doSomething(){};
 
 //////////////////////// ICEMAN //////////////////     pg 27
 Iceman::Iceman(StudentWorld* world)
-	:Actor(world, IID_PLAYER, ICEMAN_START_X, ICEMAN_START_Y, right, 1.0, 0)
+	:Actor(world,IID_PLAYER, ICEMAN_START_X, ICEMAN_START_Y, right, 1.0, 0)
 {
 	m_HP = 10;
 	m_water_amnt = 5;
 	m_sonar_amnt = 1;
 	m_gold_amnt = 0;
 }
-Iceman::~Iceman() {}
+Iceman::~Iceman(){}
 
 //first attempt to delete Ice
-//void Iceman::overlap(std::unique_ptr<Ice> iceField[60][60], Iceman* player) {
+//bool Iceman::overlap(std::unique_ptr<Ice> iceField[64][60], Iceman* player) {
 //	double playerSize = player->getSize();
 //	double iceSize = iceField[0][0]->getSize(); 
 //
 //	if (playerSize > iceSize) {
-//
+//		return true;
+//	}
+//	else {
+//		return false;
 //	}
 //}
 
 void Iceman::doSomething() {
 	//check if the iceman is alive
 	if (m_HP <= 0) {
-		setIsAlive(false);
+		setisAlive(false);
 		return;
-	}
-
+	} 
+	getWorld()->overlap();
 	//pg30
 	int ch;
 	if (getWorld()->getKey(ch) == true) {
-		switch (ch)
+		switch(ch)
 		{
-		case KEY_PRESS_LEFT:  //x-1
-			setDirection(left);
-			if (getX() > 0)
-				moveTo(getX() - 1, getY());
-			break;
-		case KEY_PRESS_RIGHT: //x+1
-			setDirection(right);
-			if(getX() < MAX_WINDOW-4)
-				moveTo(getX() + 1, getY());
-			break;
-		case KEY_PRESS_DOWN:  //y-1
-			setDirection(down);
-			if (getY() > 0)
-				moveTo(getX(), getY() - 1);
-			break;
-		case KEY_PRESS_UP: //y+1
-			setDirection(up);
-			if (getY() < MAX_WINDOW-4)
-				moveTo(getX(), getY() + 1);
-			break;
-		case KEY_PRESS_SPACE:
-			break;
-		case KEY_PRESS_TAB:
-			break;
-
+			case KEY_PRESS_LEFT:
+				setDirection(left);
+				if (getX() > 0)
+					moveTo(getX() - 1, getY());
+				break;
+			case KEY_PRESS_RIGHT:
+				setDirection(right);
+				if (getX() < MAX_WINDOW - 4)
+					moveTo(getX() + 1, getY());
+				break;
+			case KEY_PRESS_DOWN:
+				setDirection(down);
+				if (getY() > 0)
+					moveTo(getX(), getY() - 1);
+				break;
+			case KEY_PRESS_UP:
+				setDirection(up);
+				if (getY() < MAX_WINDOW - 4)
+					moveTo(getX(), getY() + 1);
+				break;
+			case KEY_PRESS_SPACE:
+				break;
+			case KEY_PRESS_TAB:
+				break;
+				
 		}
 	}
-
-
+	
+	  
 
 };
