28d6ccc0ee762deb304422a715699c933548ec8f
AndrewOki <65211402+AndrewOki@users.noreply.github.com>
2020-05-18 18:28:16 -0700

    Update StudentWorld.h

--- IceMan/IceMan/StudentWorld.h
+++ IceMan/IceMan/StudentWorld.h
@@ -11,6 +11,7 @@ const int MAX_WINDOW = 64;
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 class Ice;
+class Iceman;
 
 class StudentWorld : public GameWorld    //inherited from public gameworld
 {
@@ -19,29 +20,19 @@ public:
 		: GameWorld(assetDir)
 	{
 	}
+	//function declrations
 	void createIce();
 	void createPlayer();
+	StudentWorld* getStudentWorld();
+
 	virtual int init(){
+		//std::vector<Actor> Actors;   //pg 16
 		createIce();
 		createPlayer();
 		return GWSTATUS_CONTINUE_GAME;
 	}
 
-	virtual int move()
-	{
-		// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
-		// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-		
-		//https://www.modernescpp.com/index.php/c-core-guidelines-passing-smart-pointer
-
-		//player->overlap(std::move(iceContainer), player);
-
-
-		decLives();
-		//TEMP///
-		return GWSTATUS_CONTINUE_GAME;
-		//return GWSTATUS_PLAYER_DIED;
-	}
+	virtual int move();
 
 	virtual void cleanUp()
 	{
@@ -51,6 +42,7 @@ private:
 	Ice* ice;
 	std::unique_ptr<Ice> iceContainer[MAX_WINDOW][MAX_WINDOW-4];
 	Iceman* player;
+	StudentWorld* world;
 };
 
 #endif // STUDENTWORLD_H_
