7309614a52c1f90778b718b3648a21f3edf06dc0
kidkloss <65252893+kidkloss@users.noreply.github.com>
2020-05-20 18:37:22 -0700

    Update StudentWorld.h

--- IceMan/IceMan/StudentWorld.h
+++ IceMan/IceMan/StudentWorld.h
@@ -6,45 +6,50 @@
 #include "Actor.h"
 #include <string>
 #include <memory>
+#include <vector>
+
 
 const int MAX_WINDOW = 64;
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 class Ice;
+class Actor;
 class Iceman;
+
+
 
 class StudentWorld : public GameWorld    //inherited from public gameworld
 {
 public:
-	//constructor
 	StudentWorld(std::string assetDir)
 		: GameWorld(assetDir)
 	{
 		player = nullptr;
 	}
-	//destructor
 	~StudentWorld();
-	
-	//function declrations
+
 	void createIce();
 	void createPlayer();
-	//void createBoulder();
-	//void createProtestor();
-	//void createHardcoreProtestor();
-	//std::unique_ptr<Iceman> getPlayer() const; 
+	void createBoulder();
+	void createSquirt();
+
+	void createNormalProtestor();
+	void createHardcoreProtestor();
+
+	//std::unique_ptr<Ice> getIce(int x, int y);
+	//Iceman* getPlayer() const;
+	StudentWorld* getStudentWorld();
 	
-	//methods
 	int lvlBoulder(); //returns amount of boulders in current level
 	int lvlGold(); //returns amount of gold in current level
 	int lvlOil(); //returns amount of oil in current level
-	void overlap();
+	void overlap(); //deletes ice if player overlaps
+	bool overlapAt(int x, int y); //returns true if there is an overlap (coordinates start at bottom left of wherever we want to check)
+	bool isRoomInFront(const Actor& a); //returns true if there is room for an object in front of player
 	bool iceInFront(const Actor& a);
 	void deleteIce(int x, int y);
-	
-	StudentWorld* getStudentWorld();
-	
 
-	virtual int init(){
+	virtual int init() {
 		createIce();
 		createPlayer();
 		return GWSTATUS_CONTINUE_GAME;
@@ -53,17 +58,11 @@ public:
 	virtual int move();
 
 	virtual void cleanUp();
-	
 
 private:
-	Ice* ice; //purpose?
-	StudentWorld* world; //purpose?
-	//Iceman* player; //purpose?
-	
 	std::unique_ptr<Ice> iceContainer[MAX_WINDOW][MAX_WINDOW];
 	std::unique_ptr<Iceman> player;
 	std::vector<Actor*> actors;
-
 };
 
 #endif // STUDENTWORLD_H_
