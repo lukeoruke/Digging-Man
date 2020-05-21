0aa6fd1cc5e6a0a133935686c3a4553549cbc07e
AndrewOki <65211402+AndrewOki@users.noreply.github.com>
2020-05-20 18:51:18 -0700

    Update Actor.h

--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -6,46 +6,58 @@
 
 class StudentWorld;
 
-//////////////////////// ICE //////////////////
-class Ice : public GraphObject   //grpah object on pg 24
-{
-public:
-	Ice(int x, int y);
-	~Ice();
-};
-
-//+++++++ ACTOR ++++++++
 class Actor : public GraphObject
 {
 public:
 	Actor(StudentWorld* world, int imageID, int startX, int startY, Direction dir, double size, int depth);
 	~Actor();
 	virtual void doSomething() = 0;
+	//setters
+	bool setisAlive(bool status);
+	//getters
 	StudentWorld* getWorld() const;
-	void setIsAlive(bool status);
-	bool getIsAlive() const;
+	bool getIsAlive();
 private:
 	StudentWorld* m_world;
 	bool m_isAlive;
 };
-
-//++++++++ PERSON ++++++++
-
-//////////////////////// ICE MAN //////////////
+//////////////////////// ICE //////////////////
+class Ice : public GraphObject   //grpah object on pg 24
+{
+public:
+	Ice(int x, int y);
+	~Ice();
+	//virtual void doSomething();
+};
+//////////////////////// ICE MAN //////////////////
 class Iceman : public Actor
 {
 public:
 	Iceman(StudentWorld* world);
-	//~Iceman();
+	~Iceman();
 	virtual void doSomething();
+	//setters
+
+	//getters
 	int getOil() const;
-	//void overlap(std::unique_ptr<Ice> iceField[60][60], Iceman* player);
 private:
 	int m_HP;
 	int m_water_amnt;
 	int m_sonar_amnt;
 	int m_gold_amnt;
 	int m_oil_amnt;
+};
+
+////////////////////// BOULDER //////////////////
+class Boulder : public Actor
+{
+public:
+	Boulder(StudentWorld* world, int x, int y);
+	~Boulder();
+	virtual void doSomething();
+
+private:
+
 };
 
 //+++++++ PROTESTORS +++++++
@@ -65,7 +77,6 @@ private:
 
 };
 //------ PROTESTORS ------
-
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
