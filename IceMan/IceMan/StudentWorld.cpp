281435fef2686d5a502a1f080617e4763d3a8d6b
kidkloss <65252893+kidkloss@users.noreply.github.com>
2020-05-22 23:01:14 -0700

    Update StudentWorld.cpp

--- IceMan/IceMan/StudentWorld.cpp
+++ IceMan/IceMan/StudentWorld.cpp
@@ -26,14 +26,15 @@ int StudentWorld::move() {
 	//player->overlap(std::move(iceContainer), player);
 
 	player->doSomething();
-	if (player->getIsAlive() == true) {
+	if (player->getIsAlive()) {
 
-		if (player->getOil() < lvlOil()) {   //checks if player got the oil amnt
+		if (player->getOilAmnt() < lvlOil()) {   //checks if player got the oil amnt
 			//then do something for all actors in a for loop
 
 			for (size_t i = 0; i < actors.size(); i++)  //go thorough the actor vector and trigger its do something
 			{
-
+				if (actors.at(i) != nullptr)
+					actors.at(i)->doSomething();
 
 			}
 			if (player->getIsAlive() == false)
@@ -48,13 +49,15 @@ int StudentWorld::move() {
 			GameController::getInstance().playSound(SOUND_FINISHED_LEVEL);
 			return GWSTATUS_FINISHED_LEVEL;
 		}
+		removeDead();
 	}
+	
 	else {
 		decLives();
 		return GWSTATUS_PLAYER_DIED;
 	}
-
-	decLives();
+	
+	//decLives();
 	//TEMP///
 	return GWSTATUS_CONTINUE_GAME;
 	//return GWSTATUS_PLAYER_DIED;
@@ -71,7 +74,12 @@ void StudentWorld::cleanUp() {
 			}
 		}
 	}
-
+	std::vector<unique_ptr<Actor>>::iterator it = actors.begin();
+	for (; it != actors.end(); it++)
+	{
+		delete (*it).release();
+	}
+	actors.clear();
 }
 
 StudentWorld::~StudentWorld() {
@@ -112,12 +120,13 @@ void StudentWorld::overlap(const Actor& a) {
 		for (int y = playerY; y < playerY + 4; y++) {
 			if (iceContainer[x][y] != nullptr) {
 				deleteIce(x, y);
-				GameController::getInstance().playSound(SOUND_DIG);
+				if (a.getID() == IID_PLAYER)
+					GameController::getInstance().playSound(SOUND_DIG);
 			}
 		}
 	}
 }
-bool StudentWorld::iceInFront(const Actor &a) {
+bool StudentWorld::iceInFront(const Actor& a) {
 	int x = a.getX();
 	int y = a.getY();
 	switch (a.getDirection()) {
@@ -142,6 +151,43 @@ bool StudentWorld::iceInFront(const Actor &a) {
 	}
 	return false;
 }
+bool StudentWorld::boulderInFront(const Actor& a)
+{
+	bool ans = false;
+	int xActor = a.getX();
+	int yActor = a.getY();
+	std::vector<unique_ptr<Actor>> ::iterator it = actors.begin();
+	for (; it != actors.begin() + lvlBoulder(); ++it)
+	{	
+		int xBoulder = (*it)->getX();
+		int yBoulder = (*it)->getY();
+		switch (a.getDirection()) {
+		case GraphObject::Direction::left:
+			//if (iceContainer[x - 1][y] || iceContainer[x - 1][y + 3])
+			if ( ( xActor == xBoulder + 4 ) && ((yActor >= yBoulder - 3) && yActor <= yBoulder + 3) )
+				ans = true;
+			break;
+		case GraphObject::Direction::right:
+			//if (iceContainer[x + 4][y] || iceContainer[x + 4][y + 3])
+			if ((xActor == xBoulder - 4) && ((yActor >= yBoulder - 3) && yActor <= yBoulder + 3))
+				ans = true;
+			break;
+		case GraphObject::Direction::down:
+			//if (iceContainer[x][y - 1] || iceContainer[x + 3][y - 1])
+			if ((yActor == yBoulder + 4) && ((xActor >= xBoulder - 3) && xActor <= xBoulder + 3))
+				ans = true;
+			break;
+		case GraphObject::Direction::up:
+			//if (iceContainer[x][y + 4] || iceContainer[x + 3][y + 4])
+			if ((yActor == yBoulder - 4) && ((xActor >= xBoulder - 3) && xActor <= xBoulder + 3))
+				ans = true;
+			break;
+		default:
+			return false;
+		}
+	}
+	return ans;
+}
 bool StudentWorld::overlapAt(int x, int y) {
 
 
@@ -156,29 +202,30 @@ bool StudentWorld::overlapAt(int x, int y) {
 }
 
 bool StudentWorld::isRoomInFront(const Actor& a) {
+	bool ans = false;
 	int x = a.getX();
 	int y = a.getY();
 	switch (a.getDirection()) {
 	case GraphObject::Direction::left:
 		if (!overlapAt(x - 4, y))
-			return true;
+			ans = true;
 		break;
 	case GraphObject::Direction::right:
 		if (!overlapAt(x + 4, y))
-			return true;
+			ans = true;
 		break;
 	case GraphObject::Direction::down:
 		if (!overlapAt(x, y - 4))
-			return true;
+			ans = true;
 		break;
 	case GraphObject::Direction::up:
 		if (!overlapAt(x, y + 4))
-			return true;
+			ans = true;
 		break;
 	default:
 		return false;
 	}
-	return false;
+	return ans;
 }
 
 
@@ -192,6 +239,26 @@ void StudentWorld::createPlayer() {
 	player = std::unique_ptr<Iceman>(new Iceman(this));
 }
 
+///////////////////SQUIRT/////////////////
+void StudentWorld::createSquirt(Iceman &man) {
+	int x = man.getX();
+	int y = man.getY();
+	switch (man.getDirection()) {
+	case GraphObject::Direction::left:
+		x = x - 4;
+		break;
+	case GraphObject::Direction::right:
+		x = x + 4;
+		break;
+	case GraphObject::Direction::down:
+		y = y - 4;
+		break;
+	case GraphObject::Direction::up:
+		y = y + 4;
+		break;
+	}
+	actors.emplace_back(unique_ptr<Squirt>(new Squirt(this, x, y, man.getDirection())));
+}
 
 //Iceman* StudentWorld::getPlayer() const {
 //	return player;
@@ -200,13 +267,26 @@ void StudentWorld::createPlayer() {
 ///////////////////BOUDLER/////////////////
 void StudentWorld::createBoulder(int create) {
 	for (int k = 0; k < create; k++) {
-		int x = generateRandX();
-		int y = generateRandY();
-		boulder = new Boulder(this,x,y);
+		int x = 0;
+		int y = 0;
+		do {
+			do {
+				x = generateRandX();
+			} while (x > 26 && x < 34);
 
+			do {
+				y = generateRandY();
+			} while (y < 20);
+		
+
+		} while (!distance(x, y));
+
+		unique_ptr<Boulder> boulder;
+		boulder = unique_ptr<Boulder>(new Boulder(this, x, y));
 		overlap(*boulder);
+		actors.emplace_back(std::move(boulder));
 	}
-	
+
 }
 
 int StudentWorld::lvlBoulder() {
@@ -214,10 +294,65 @@ int StudentWorld::lvlBoulder() {
 }
 
 int StudentWorld::generateRandX() {
-	return (rand() % 64);
+	return (rand() % 60);
+	
 }
 
 int StudentWorld::generateRandY() {
-	return (rand() & 60);
+	return (rand() % 56);
 }
 
+bool StudentWorld::distance(int x, int y) {
+	if (actors.size() == 0) {   //temp
+		return true;
+	}
+	std::vector<unique_ptr<Actor>> ::iterator it = actors.begin();
+	for (; it != actors.end(); it++) {
+		double d = (sqrt(pow(x - (*it)->getX(), 2) + pow(y - (*it)->getY(), 2)));
+		if (d <= 6) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void StudentWorld::removeDead() {
+	if (actors.size() == 0)
+		return;
+	
+	std::vector<unique_ptr<Actor>> ::iterator it = actors.begin();
+	for (; it != actors.end(); it++)
+	{
+		if ((*it) != nullptr) {
+
+			if (!(*it)->getIsAlive())
+			{
+				(*it).reset();
+
+			}
+		}
+	}
+	return;
+}
+
+void StudentWorld::setDisplayText() {
+	unsigned int level = getLevel();
+	unsigned int lives = getLives();
+	int health = (*player).getHP();
+	int squirts = (*player).getWaterAmnt();
+	int gold = (*player).getGoldAmnt();
+	int barrelsLeft = lvlOil() - (*player).getOilAmnt();
+	int sonar = (*player).getSonarAmnt();
+	int score = getScore();
+	// Next, create a string from your statistics, of the form:  // Lvl: 52 Lives: 3 Hlth: 80% Wtr: 20 Gld: 3 Oil Left: 2 Sonar: 1 Scr: 321000 
+
+	string s = formatStats(level, lives, health, squirts, gold, barrelsLeft, sonar, score);
+	// Finally, update the display text at the top of the screen with your newly created stats 
+	setGameStatText(s); // calls our provided GameWorld::setGameStatText
+}
+
+string StudentWorld::formatStats(unsigned int level, unsigned int lives, int health, int squirts, int gold, int barrelsLeft, int sonar, int score)
+{
+	//TODO:: format
+	return string();
+}
