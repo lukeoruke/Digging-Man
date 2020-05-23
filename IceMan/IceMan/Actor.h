822bc91f70c21034ee5fc10afdf5f91ea2d338a7
kidkloss <65252893+kidkloss@users.noreply.github.com>
2020-05-22 23:01:32 -0700

    Update Actor.h

--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -17,6 +17,8 @@ public:
 	//getters
 	StudentWorld* getWorld() const;
 	bool getIsAlive();
+	//methods
+	void moveInDirection();
 private:
 	StudentWorld* m_world;
 	bool m_isAlive;
@@ -27,7 +29,6 @@ class Ice : public GraphObject   //grpah object on pg 24
 public:
 	Ice(int x, int y);
 	~Ice();
-	//virtual void doSomething();
 };
 //////////////////////// ICE MAN //////////////////
 class Iceman : public Actor
@@ -39,13 +40,29 @@ public:
 	//setters
 
 	//getters
-	int getOil() const;
+	int getHP() const;
+	int getWaterAmnt() const;
+	int getSonarAmnt() const;
+	int getGoldAmnt() const;
+	int getOilAmnt() const;
+
 private:
 	int m_HP;
 	int m_water_amnt;
 	int m_sonar_amnt;
 	int m_gold_amnt;
 	int m_oil_amnt;
+};
+
+class Squirt : public Actor
+{
+public:
+	Squirt(StudentWorld* world, int row, int col, Direction direction);
+	~Squirt();
+	virtual void doSomething();
+	
+private:
+	int m_travel_distance;
 };
 
 ////////////////////// BOULDER //////////////////
